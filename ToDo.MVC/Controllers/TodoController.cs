380ce830dbf3570004ae28ab20b4dce66e7a6684
Usman Idris <kwaru91@gmail.com>
2023-03-25 17:16:26 +0100

    Add Todo Controller

---
+++ ToDo.MVC/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.MVC.Models;
+
+namespace ToDo.MVC.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoDbContext _dbContext;
+
+        public TodoController(TodoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var todos = _dbContext.Todos.ToList();
+            return View(todos);
+        }
+    }
+}
