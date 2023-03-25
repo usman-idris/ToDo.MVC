e0be653129a2538662cc9d0361e6089a86d9cf49
Usman Idris <kwaru91@gmail.com>
2023-03-25 16:53:48 +0100

    Add Seed Data

---
+++ ToDo.MVC/Models/TodoDbSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.MVC.Models
+{
+    public class TodoDbSeeder
+    {
+        public static void Seed(IServiceProvider serviceProvider)
+        {
+            using var context = new TodoDbContext(serviceProvider.GetRequiredService<DbContextOptions<TodoDbContext>>());
+
+            //Look for any Todos.
+            if (context.Todos.Any())
+            {
+                //If there is any seeded data already, then return
+                return;
+            }
+
+            context.Todos.AddRange(
+                new Todo
+                {
+                    Id = 1,
+                    TaskName = "Work on book Chapter",
+                    IsComplete = false,
+                },
+
+                new Todo
+                {
+                    Id = 2,
+                    TaskName = "Create video content",
+                    IsComplete = false,
+                }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
