d131a7267ace0baec31b6c7c2add3c3a3c48db3b
Usman Idris <kwaru91@gmail.com>
2023-03-26 22:01:53 +0100

    Add CRUD operations

--- ToDo.MVC/Controllers/TodoController.cs
+++ ToDo.MVC/Controllers/TodoController.cs
@@ -18,5 +18,59 @@ namespace ToDo.MVC.Controllers
             var todos = _dbContext.Todos.ToList();
             return View(todos);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Todo todo)
+        {
+            var todoId = _dbContext.Todos.Select(x => x.Id).Max() + 1;
+            todo.Id = todoId;
+            _dbContext.Todos.Add(todo);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var todo = _dbContext.Todos.Find(id);
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Todo todo)
+        {
+            _dbContext.Todos.Update(todo);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var todo = _dbContext.Todos.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var todo = _dbContext.Todos.Find(id);
+            _dbContext.Todos.Remove(todo);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
